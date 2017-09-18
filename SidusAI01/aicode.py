1fec4e0aed0063066764d5876c7910cb32292345
Mingi Shin <yuagnun@gmail.com>
2017-09-18 17:42:34 +0900

    Minor refactoring

--- SidusAI01/aicode.py
+++ SidusAI01/aicode.py
@@ -2,18 +2,31 @@ from ailib import *
 from ainetwork import *
 import random
 
+verbose = 1
+game_data = {}
 
-
-def Init(height, width, cooldownMirror, cooldownLaser, gameTurn):
-    print(height, width, cooldownMirror, cooldownLaser, gameTurn)
-
-def AI(board, P1robots, P2robots):
-    
+def move_random(board, P1robots, P2robots):
     ret = []
-    
     for i in P1robots:
         posAction = [ACTION.MOVE,
-		ACTION.PLACEMIRROR1, ACTION.PLACEMIRROR2, ACTION.SHOT, ACTION.NOTHING]
+        ACTION.PLACEMIRROR1, ACTION.PLACEMIRROR2, ACTION.SHOT, ACTION.NOTHING]
         dirAction = [DIRECTION.LEFT, DIRECTION.RIGHT, DIRECTION.UP, DIRECTION.DOWN]
         ret.append(RobotMove(random.choice(posAction),random.choice(dirAction)))
     return ret
+
+def Init(height, width, cooldownMirror, cooldownLaser, gameTurn):
+    print(height, width, cooldownMirror, cooldownLaser, gameTurn)
+    game_data['H'] = height
+    game_data['W'] = width
+    game_data['CoolM'] = cooldownMirror
+    game_data['CoolL'] = cooldownLaser
+    game_data['Turn'] = gameTurn
+    game_data['turn'] = game_data['Turn']
+
+def AI(board, P1robots, P2robots):
+    move = []
+    move = move_random(board, P1robots, P2robots)
+    game_data['turn'] -= 1
+    if verbose:
+        pass
+    return move
